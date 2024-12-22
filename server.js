5b29b478aba80884a614de4a4f3c4396d56008be
Alien <minhhnghiii@gmail.com>
2024-12-22 13:06:04 +0700

    done resolver, typedefs

---
+++ server.js
@@ -0,0 +1,29 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+//server setup
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+  },
+};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
